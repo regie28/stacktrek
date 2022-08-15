fc8e0b0986a7702b0ea4a5c844e233917f5e700f
Reginald Bartiana <ghostrider072017@gmail.com>
2022-08-15 13:09:52 +0800

    changes

--- JS/Chapter_2/Converter/converter.js
+++ JS/Chapter_2/Converter/converter.js
@@ -1,8 +1,19 @@
 let btn = document.querySelector('button');
 
-btn.addEventListener('click', function() {
-    let c = document.querySelector("#celsius");
-    let f = document.querySelector("#fahrenheit");
-    f.value = c.value * 9 / 5 + 32 + "\xB0F";
+btn.addEventListener('click', (e) => {
+  let c = document.querySelector('#celsius');
+  let f = document.querySelector('#fahrenheit');
+  f.value = (c.value * 9) / 5 + 32 + '\xB0F';
 });
-
+/*
+document.querySelector('button').onclick = function () {
+  let c = document.querySelector('celsius');
+  let f = (c.value * 9) / 5 + 32 + '\xB0F';
+  let degrees = f.value + '\xB0F';
+  if (c > 0) {
+    return degrees;
+  } else {
+    return f.value;
+  }
+  document.querySelector('fahrenheit').innerHTML = degrees;
+};*/
